b48710f0f4b19bd994e6cea1f81b50b5c3c4388a
Shankar Lohar <shankarloharmail@gmail.com>
2023-06-03 12:59:04 +0530

    TASK 1 COMPLETE

--- Task1/app/src/main/java/shankar/lohar20BAI10040/task1/MainActivity.kt
+++ Task1/app/src/main/java/shankar/lohar20BAI10040/task1/MainActivity.kt
@@ -3,41 +3,71 @@ package shankar.lohar20BAI10040.task1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import shankar.lohar20BAI10040.task1.ui.theme.Task1Theme
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Task1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+
+                Column(
+                ) {
+                    var username by remember {
+                        mutableStateOf("")
+                    }
+                    var password by remember {
+                        mutableStateOf("")
+                    }
+                    Image(
+                        painter = painterResource(R.drawable.vit_logo),
+                        contentDescription = "vit logo image",
+                    )
+                    Spacer(modifier = Modifier.padding(20.dp))
+                    Text(text = "Username")
+                    TextField(value = username, onValueChange = { username = it })
+                    Spacer(modifier = Modifier.padding(16.dp))
+                    Text(text = "Password")
+                    TextField(value = password, onValueChange = { password = it })
                 }
+                Spacer(modifier = Modifier.padding(20.dp))
+                Row(
+
+                ) {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Sign In")
+                    }
+                    Spacer(modifier = Modifier.padding(20.dp))
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Sign Up")
+                    }
+                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Task1Theme {
-        Greeting("Android")
-    }
-}
